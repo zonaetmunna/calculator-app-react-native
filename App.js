89dc7e6c5f077f015b14f852a48e8bb170fb3b48
MD. Zonaet Hossain Munna <zonaetmonna@gmail.com>
2024-09-03 19:20:54 +0600

    fiixed design cacculator

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet } from 'react-native';
 import FlashMessage from 'react-native-flash-message';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { ThemeProvider } from './context/ThemeContext';
-import Navigation from './navigation';
+import { ThemeProvider } from './src/context/ThemeContext';
+import Navigation from './src/navigation';
 
 export default function App() {
 	let [fontsLoaded] = useFonts({
-		'Manrope-Bold': require('./assets/fonts/Manrope-Bold.ttf'),
-		'Manrope-Regular': require('./assets/fonts/Manrope-Regular.ttf'),
-		'Manrope-Medium': require('./assets/fonts/Manrope-Medium.ttf'),
+		'Manrope-Bold': require('./src/assets/fonts/Manrope-Bold.ttf'),
+		'Manrope-Regular': require('./src/assets/fonts/Manrope-Regular.ttf'),
+		'Manrope-Medium': require('./src/assets/fonts/Manrope-Medium.ttf'),
 	});
 	if (!fontsLoaded) {
 		return null;
